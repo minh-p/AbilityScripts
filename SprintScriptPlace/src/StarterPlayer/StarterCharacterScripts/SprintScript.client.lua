e1c50fbf338448aaef1944dde369dd3d1eeda562
minh-p <minhisverynormal@gmail.com>
2021-03-13 16:39:21 -0800

    Added the Sprint Script

---
+++ SprintScriptPlace/src/StarterPlayer/StarterCharacterScripts/SprintScript.client.lua
@@ -0,0 +1,39 @@
+-- About: A sprinting script. Very easy
+-- Developer: Minhnormal
+-- Date started 3/13/2021
+
+local Players = game:GetService("Players")
+local ContextActionService = game:GetService("ContextActionService")
+
+local localPlayer = Players.LocalPlayer
+
+local SPRINT_ACTION_NAME = "Sprinting"
+
+local SPEED_WHEN_SPRINTING = 20
+local SPEED_WHEN_WALKING = 16
+
+local sprintKeybind = {
+    desktop = Enum.KeyCode.LeftShift
+}
+
+
+local function handleSprinting(_, inputState, _)
+    -- Declare the player character again because the player will respawn
+    -- and the character will be new
+
+    local playerCharacter = localPlayer.Character or localPlayer.CharacterAdded:Wait()
+    local playerHumanoid = playerCharacter:WaitForChild("Humanoid")
+
+    if inputState == Enum.UserInputState.Begin then
+        playerHumanoid.WalkSpeed = SPEED_WHEN_SPRINTING
+    elseif inputState == Enum.UserInputState.End then
+        playerHumanoid.WalkSpeed = SPEED_WHEN_WALKING
+    end
+end
+
+
+local function setupSprinting()
+    ContextActionService:BindAction(SPRINT_ACTION_NAME, handleSprinting, true, sprintKeybind.desktop)
+end
+
+setupSprinting()
